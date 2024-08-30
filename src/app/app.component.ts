dc1ae521f41ef9fd75d5b8e1a4ea1a1737349925
sarvsav <sarvsav@gmail.com>
2024-08-30 18:56:22 +0200

    Added theme component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,39 @@
-import { Component } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
+import { Component, inject } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
+import { InputSwitchModule } from 'primeng/inputswitch';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, InputSwitchModule, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'go-emr-fe';
+
+  constructor() {
+    if(this.isSystemDark()) {
+      this.toggleLightDark();
+    }
+  }
+
+  isSystemDark(): boolean {
+    return window?.matchMedia?.('(prefers-color-scheme:dark)')?.matches;
+  }
+
+  #document = inject(DOCUMENT)
+  isDarkMode = false;
+  toggleLightDark() {
+    const linkElement = this.#document.getElementById('app-theme') as HTMLLinkElement;
+    if (linkElement.href.includes('light')) {
+      linkElement.href = 'theme-dark.css';
+      this.isDarkMode = true;
+    } else {
+      linkElement.href = 'theme-light.css';
+      this.isDarkMode = false;
+    }
+  }
 }
